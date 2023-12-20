1e10a098bcf9c183bfc334a3b068ec22c26323d6
John Hansen <jhb.rgn@gmail.com>
2023-12-20 11:18:31 -0500

    Style landing page, set-up OAuth and Passport

---
+++ controllers/inouts.js
@@ -0,0 +1,21 @@
+
+
+
+async function create(req, res) {
+    const movie = await Movie.findById(req.params.id);
+  
+    // Add the user-centric info to req.body (the new review)
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
+  
+    // We can push (or unshift) subdocs into Mongoose arrays
+    movie.reviews.push(req.body);
+    try {
+      // Save any changes made to the movie doc
+      await movie.save();
+    } catch (err) {
+      console.log(err);
+    }
+    res.redirect(`/movies/${movie._id}`);
+  }
