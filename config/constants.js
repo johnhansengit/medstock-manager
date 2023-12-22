24a8095426b0befaee80ee0d83f42ba187b86abf
John Hansen <jhb.rgn@gmail.com>
2023-12-22 16:59:25 -0500

    Set up family calsses

--- config/constants.js
+++ config/constants.js
@@ -1,19 +1,17 @@
 const families = [
-    'Analgesics/Antipyretics',
-    'Antibiotics (Oral)',
-    'Antiemetics',
-    'Anti-Diarrheals',
-    'Antihistimines',
-    'Antihypertensives',
-    'Corticosteroids (Oral)',
-    'Diarrhetics',
-    'Emergency Drugs',
-    'IV Fluids',
-    'Laxitives/Stool Softeners',
-    'Topicals & Drops',
-    'Vaccines',
-    'Lab',
-    'Consumables'
+    {name: 'Analgesics/Antipyretics', class: 'c1'},
+    {name: 'Antibiotics (Oral)', class: 'c2'},
+    {name: 'Antiemetics', class: 'c3'},
+    {name: 'Anti-Diarrheals', class: 'c4'},
+    {name: 'Antihistimines', class: 'c5'},
+    {name: 'Antihypertensives', class: 'c6'},
+    {name: 'Corticosteroids (Oral)', class: 'c7'},
+    {name: 'Diarrhetics', class: 'c8'},
+    {name: 'Emergency Drugs', class: 'c9'},
+    {name: 'IV Fluids', class: 'c10'},
+    {name: 'Laxitives/Stool Softeners', class: 'c11'},
+    {name: 'Topicals & Drops', class: 'c12'},
+    {name: 'Vaccines',  class: 'c13'}
 ];
 
 const depletions = [
